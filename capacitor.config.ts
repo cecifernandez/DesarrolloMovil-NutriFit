0a7b3cbca88a0d150093ed60c27768175144ec4f
mceciliafernandezst@gmail.com <mceciliafernandezst@gmail.com>
2025-10-30 00:17:25 -0300

    apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nutrifit',
+  webDir: 'www'
+};
+
+export default config;
